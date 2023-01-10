4b2eeb5a6caf78134a10e0d6fb7d819dfe4f625e
myselftanveer <myselftanveer@gmail.com>
2023-01-10 18:09:39 +0530

    excel added1

--- TestData/HomePageData.py
+++ TestData/HomePageData.py
@@ -8,7 +8,7 @@ class HomePageData:
     @staticmethod
     def getTestData(test_case_name):
         Dict = {}
-        book = openpyxl.load_workbook("pythonDemo.xlsx")
+        book = openpyxl.load_workbook("TestData\\pythonDemo.xlsx")
         sheet = book.active
         for i in range(1, sheet.max_row + 1):  # to get rows
             if sheet.cell(row=i, column=1).value == test_case_name:
